54e90b1fe3647f9bd367cf344ae187cee4eeae7b
mendesana99 <wasertattoo@outlook.com>
2026-04-21 18:26:55 -0300

    feat(Domain): implementacion de las reglas de excepcion del dominio y limpieza de interfaces

--- Trabajo_ps/Program.cs
+++ Trabajo_ps/Program.cs
@@ -1,31 +1,34 @@
 using Application;
 using Infrastructure;
 using Infrastructure.Data;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; //para manejar sql desde c#
 
+// inicia el constructor del servidor web. Es la base de toda la app.
 var builder = WebApplication.CreateBuilder(args);
 
-// Agregar servicios
+// Agregar servicios al contenedor - get, post, urls, swagger.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configurar Inyección de Dependencias
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication(); //carga servicios con application
+builder.Services.AddInfrastructure(builder.Configuration); //lee las pass/links de bd
 
-// CORS para el frontend
+// CORS para el frontend - permite que navegadores consulten la api
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
+        //permite peticiones de web externa cualquiera 
         policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
     });
 });
 
+//construye la app
 var app = builder.Build();
 
-// Crear base de datos y cargar datos de prueba
+// Crear base de datos y cargar datos de prueba "espacio temporal" 
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -33,15 +36,18 @@ using (var scope = app.Services.CreateScope())
     await SeedData.InitializeAsync(scope.ServiceProvider);
 }
 
-// Middleware
+// Tubería de peticiones http
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowAll");
-app.UseAuthorization();
-app.MapControllers();
+app.UseDefaultFiles(); //index
+app.UseStaticFiles(); //app.js , styles.css o img
+app.UseCors("AllowAll"); //activa el guardia de frontera creado 
+app.UseAuthorization(); //agrega capa seguridad
+app.MapControllers(); //conecta rutas web conc controllers en c#
 
+//prende server y espera peticiones
 app.Run();
